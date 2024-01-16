039674fb954bedb1f8c773c9142b1ab60f1ade23
wired <filip.piatkowski.gdy@gmail.com>
2024-01-16 22:19:49 +0100

    Added Calculator class with actions; add, subtract, multiply and divide.

---
+++ Calculator/src/main/java/org/example/Calculator.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Calculator {
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public double divide(double num1, double num2) throws  ArithmeticException {
+        if (num2 == 0) {
+            throw new ArithmeticException("Division by zero is not allowed at this time.");
+        }
+        return num1 / num2;
+    }
+}
